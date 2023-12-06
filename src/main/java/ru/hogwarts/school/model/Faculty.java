c5658106719b6f26c8d43cb271e45980efaed3cd
Sergey Volkov <rusobraz@mail.ru>
2023-12-06 21:50:00 +0300

    Шаг №2, 3

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -1,14 +1,42 @@
 package ru.hogwarts.school.model;
 
+import java.util.Objects;
+
 public class Faculty {
-    private Long id;
-    private String name;
-    private String color;
+    private long id;
+    private String name, color;
+
+    public Faculty() {
+    }
 
     public Faculty(long id, String name, String color) {
         this.id = id;
         this.name = name;
         this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Faculty faculty = (Faculty) o;
+        return id == faculty.id && Objects.equals(name, faculty.name) && Objects.equals(color, faculty.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
     }
 
     public long getId() {
